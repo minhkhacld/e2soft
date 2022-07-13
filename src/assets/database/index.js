86cd521f131764b82699acaf2d0afc6df18a38bc
minhkhacld <you@example.com>
2022-07-13 07:21:11 +0700

    add databae

--- src/assets/database/index.js
+++ src/assets/database/index.js
@@ -1,4 +1,4 @@
-export { default as irregular } from './category/dt_bat_quy_tac';
+export { default as irregular } from './category/voca_irregula';
 export { default as oxford_3000_words } from './category/three_thoundsand_words';
 export { default as sewing_voca } from './category/sewing';
 export { default as mechanical_voca } from './category/mechanical';
